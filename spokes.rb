ac4032ef64cb961c5fc16780aef6b2d2e7709549
John Borak <john.borak@gmail.com>
2021-02-11 14:42:04 -0500

    updated hash

--- spokes.rb
+++ spokes.rb
@@ -2,7 +2,7 @@ class Spokes < Formula
   desc "Spokes is an HTTP and TCP tunneling server."
   homepage "https://packetriot.com/enterprise"
   url "https://download.packetriot.com/linux/tarballs/spokes-1.0.5.tar.gz"
-  sha256 "1fc44c799e1fe11fb923a2d6773b82c3401b4c9d122000686cc75f08c65dcd41"
+  sha256 "d2f9b21a222524d73069278367a4262caa0e8709310ec0fce81308a26059827f"
   license ""
 
   bottle :unneeded
@@ -11,7 +11,7 @@ class Spokes < Formula
     # ENV.deparallelize  # if your formula fails when building in parallel
     # Remove unrecognized options if warned by configure
     bin.install "usr/bin/spokes"
-    system "install.sh", "#{prefix}"
+    system "install.sh"
   end
 
   def uninstall
